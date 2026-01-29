900e2d9116515d62448f3bef2c36f45d9bf8845c
moha-dev94 <ennadifima@gmail.com>
2026-01-29 10:35:31 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -3,39 +3,40 @@
 int main() 
 {
 
-  printf("Taille types de base en octets :\n");
-  printf("================================= : \n");
+  printf("Taille types de base en octets :");
+  printf("================================= : \);
 
   //1. char
-  printf("char   : %zu\n", sizeof(char));
-  printf("unsigned char : zu%\n", sizeof(unsigned char));
+  printf("char   : %zu\, sizeof(char));
+  printf("unsigned char : zu%", sizeof(unsigned char));
 
     //2. short
-  printf("short   : %zu\n", sizeof(short));
-  printf("unsigned short : zu%\n", sizeof(unsigned short));
+  printf("short   : %zu", sizeof(short));
+  printf("unsigned short : zu%", sizeof(unsigned short));
 
     //3. int
-  printf("int   : %zu\n", sizeof(int));
-  printf("unsigned int : zu%\n", sizeof(unsigned int));
+  printf("int   : %zu", sizeof(int));
+  printf("unsigned int : zu%", sizeof(unsigned int));
 
     //4. long int
-  printf("long int   : %zu\n", sizeof(long int));
-  printf("unsigned long int : zu%\n", sizeof(unsigned long int));
+  printf("long int   : %zu", sizeof(long int));
+  printf("unsigned long int : zu%", sizeof(unsigned long int));
 
     //5. long long int
-  printf("long long int   : %zu\n", sizeof(long long int));
-  printf("unsigned long long int : zu%\n", sizeof(unsigned long long int));
+  printf("long long int   : %zu", sizeof(long long int));
+  printf("unsigned long long int : zu%", sizeof(unsigned long long int));
 
     //6. float
-  printf("float   : %zu\n", sizeof(float));
+  printf("float   : %zu", sizeof(float));
 
     //7. double
-  printf("double   : %zu\n", sizeof(double));
+  printf("double   : %zu", sizeof(double));
 
     //8. long double
-  printf("long double   : %zu\n", sizeof(long double));
+  printf("long double   : %zu", sizeof(long double));
 
   return 0;
   
 }
+
 
